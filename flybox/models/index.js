362d2831c6174daffa87fd45cf0fbfa7b17fb1cb
shockeyt <shockeyt@gmail.com>
2017-05-02 10:37:34 -0600

    heroku deploy try

--- flybox/models/index.js
+++ flybox/models/index.js
@@ -1,4 +1,7 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/flybox');
+mongoose.connect( process.env.MONGODB_URI ||
+				  process.env.MONGOLAB_URI ||
+				  process.env.MONGOHQ_URL ||
+				  'mongodb://localhost/flybox');
 
 module.exports.Fly = require('./fly.js');
